949f5c0385e640727ce23b62b5a5753405c9338d
Shaun Earley <sme3750@gmail.com>
2022-10-28 10:34:49 -0700

    adding my name as the store

--- pages/index.js
+++ pages/index.js
@@ -13,7 +13,7 @@ export default function Home() {
 
       <main className={styles.main}>
         <h1 className={styles.title}>
-          Welcome to Shaun's Store
+          Welcome to Shauns Store
           using <a href="https://nextjs.org">Next.js!</a>
         </h1>
 
